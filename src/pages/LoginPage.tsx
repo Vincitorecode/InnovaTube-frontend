a6f21c5c06f160ce147d7e4732ab09574b3aaa0c
Victor <vincitore.code@gmail.com>
2025-06-27 00:31:43 -0600

    feat(): local for vite api

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -40,14 +40,15 @@ const LoginPage = () => {
 
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: form.emailOrUsername,
-          password: form.password,
-        }),
-      });
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: form.emailOrUsername,
+        password: form.password,
+      }),
+    });
+
 
       const data = await res.json();
 
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -51,16 +51,16 @@ const RegisterPage = () => {
     if (!validateForm()) return;
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          name: form.name,
-          username: form.username,
-          email: form.email,
-          password: form.password,
-        }),
-      });
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              name: form.name,
+              username: form.username,
+              email: form.email,
+              password: form.password,
+            }),
+          });
 
       if (!res.ok) {
         const data = await res.json();
