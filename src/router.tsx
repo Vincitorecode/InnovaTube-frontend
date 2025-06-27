f8bb8e3987eb8b5444b660cfe2ecc395c2183d14
Victor <vincitore.code@gmail.com>
2025-06-26 23:51:05 -0600

    feat(auth): redirect to login by default

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 
-
 export const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
-      
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
     </Routes>
   </BrowserRouter>
 );
