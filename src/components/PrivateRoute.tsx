9574d878de0bd6d2d71958896dd778ef45741f20
Victor <vincitore.code@gmail.com>
2025-06-27 01:39:38 -0600

    fetch(element jsx)

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,11 +1,9 @@
 
 import { Navigate } from "react-router-dom";
-import { ReactNode } from "react";
 
-const PrivateRoute = ({ children }: { children: ReactNode }) => {
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/login" replace />;
 };
-
 
 export default PrivateRoute;
--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import PrivateRoute from "./components/PrivateRoute";
 
 export const AppRoutes = () => (
-   <BrowserRouter>
+  <BrowserRouter>
     <Routes>
+      {/* Redirección inicial al login */}
+      <Route path="/" element={<Navigate to="/login" />} />
+      
+      {/* Rutas públicas */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      
+
+      {/* Ruta protegida */}
       <Route
-        path="/"
+        path="/home"
         element={
           <PrivateRoute>
             <HomePage />
